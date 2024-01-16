6a2983eb4d6c21250f3c501ebf7c9d09f0862afa
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-16 11:14:59 +0200

    Deleted unwanted space

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ LastDigit = number % 10
 print("Last digit of", number, "is", end=" ")
 
 if LastDigit == 0:
-    print(LastDigit,"and is 0")
+    print(LastDigit, "and is 0")
 elif number > 0 and LastDigit > 5:
     print(LastDigit, "and is greater than 5")
 elif number < 0 and LastDigit < 6 and LastDigit != 0:
