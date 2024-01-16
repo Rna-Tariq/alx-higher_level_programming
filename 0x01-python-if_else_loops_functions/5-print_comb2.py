10426c004bc9af38014b4759ccfed3bcdea107d6
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-16 11:48:45 +0200

    Added new line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i >= 0 and i < 10:
-        print("0{},".format(i), end=" ")
-    else:
-        print("{},".format(i), end=" ")
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
