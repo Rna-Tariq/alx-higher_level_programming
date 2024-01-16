ed6f27c95eaf324d0e857e0d88863ce26a97b641
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-16 18:11:13 +0200

    Insert in singly Linkedlist

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,43 +1,42 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * insert_node - function that inserts a number to a singly linked list.
- * @head: pointer to pointer to head of the list.
- * @number: value of the node to insert.
+ * insert_node - Inserts a number into a sorted singly linked list
+ * @head: Pointer to the head of the list
+ * @number: The number to insert
  *
- * Return: the address of the new node, or NULL if it failed.
+ * Return: Address of the new node, or NULL if it failed
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *traverse;
-	unsigned int idx = 0, i = 0;
-	if (head == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = number;
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	traverse = *head;
-	while (i != idx - 1 && traverse != NULL)
-	{
-		traverse = traverse->next;
-		i++;
-	}
+    listint_t *new_node, *current, *prev;
 
-	if (i == idx - 1 && traverse != NULL)
-	{
-		new_node->next = traverse->next;
-		traverse->next = new_node;
-		return (new_node);
-	}
-	return (NULL);
+    new_node = malloc(sizeof(listint_t));
+    if (!new_node)
+        return NULL;
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    if (*head == NULL || number < (*head)->n)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return new_node;
+    }
+
+    current = *head;
+    prev = NULL;
+
+    while (current && current->n < number)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    prev->next = new_node;
+    new_node->next = current;
+
+    return new_node;
 }
