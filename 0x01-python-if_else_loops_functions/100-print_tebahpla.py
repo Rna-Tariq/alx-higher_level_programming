78b54081966a8321a292f5b66c7a675919814cae
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-16 18:20:27 +0200

    Enhanced spaces

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(i if (i % 2 == 0) else (i - 32)), end='')
+for char in range(122, 96, -1):
+    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end='')
