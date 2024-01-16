98b4c9650405cc2da6c7203728b39fe4a90307bb
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-16 16:41:42 +0200

    no new line

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 def print_last_digit(number):
     number = abs(number)
     last_digit = number % 10
-    print("{}".format(last_digit))
+    print("{}".format(last_digit), end='')
     return last_digit
