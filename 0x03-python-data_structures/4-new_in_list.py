05f7546c85a0fde5bbef61a043da72b7a13c4b89
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-21 18:11:57 +0200

    used copy()

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        new_list = my_list
+        new_list = my_list.copy()
         if idx < 0 or idx > len(new_list) - 1:
             return new_list
         else:
