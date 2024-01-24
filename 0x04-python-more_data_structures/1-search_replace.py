84dcea0ca5cf60823efc7d1f13e211f78c1b02e1
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-24 12:42:47 +0200

    Deleted validation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     copyList = []
-    if my_list:
-        for i in range(len(my_list)):
-            if my_list[i] == search:
-                copyList.append(replace)
-            else:
-                copyList.append(my_list[i])
-        return copyList
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copyList.append(replace)
+        else:
+            copyList.append(my_list[i])
+    return copyList
