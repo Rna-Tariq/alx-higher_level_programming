3e0141fc846717c2b2de4f215daa604c2e5ae94d
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-24 12:38:32 +0200

    Replace element enhanced

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    copyList = []
     if my_list:
         for i in range(len(my_list)):
             if my_list[i] == search:
-                my_list[i] = replace
-        return my_list
+                copyList.append(replace)
+            else:
+                copyList.append(my_list[i])
+        return copyList
