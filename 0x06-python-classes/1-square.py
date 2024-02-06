52d68b6e642c23bfdf640ec97bfd2c19193877a8
Rna341 <rna.tariq.aboouf@gmail.com>
2024-02-06 14:37:41 +0200

    Created a constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class."""
+
+class Square:
+    """Define a class Square."""
+    def __init__(self, size):
+
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+
+        self.__size = size
