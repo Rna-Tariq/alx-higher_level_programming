12d7ba3687db53e9af10c4c02389b24c9217b692
Rna341 <rna.tariq.aboouf@gmail.com>
2024-02-06 15:26:35 +0200

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,13 +4,14 @@
 
 class Square:
     """Define a class Square."""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position = (0, 0)):
 
         """initialize square
         Args:
             size (int): size of the square
         """
         self.__size = size
+        self.position = position
 
     def area(self):
         """calculates the area.
@@ -18,7 +19,7 @@ class Square:
         Returns:
             area.
         """
-        return self.__size**2
+        return (self.__size * self.__size)
 
     @property
     def size(self):
@@ -38,6 +39,20 @@ class Square:
         else:
             self.__size = new_size
 
+    @property
+    def position(self):
+        """Getset the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def my_print(self):
         """prints in stdout the square with the character #"""
         if self.__size == 0:
@@ -49,4 +64,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
