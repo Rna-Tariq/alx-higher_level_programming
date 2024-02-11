63c99a71d3b99c352a616160737cdf268b650fc3
Rna341 <rna.tariq.aboouf@gmail.com>
2024-02-11 16:19:54 +0200

    Handle indentations

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join([line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
