d31b7d388cb490abe061a259c8f70d845ee397ea
Rna341 <rna.tariq.aboouf@gmail.com>
2024-04-23 00:01:26 +0200

    Fixed if statement

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 // prints a square
 
-if (isNaN(parseInt(process.argv[2]))) {
+if (isNaN(process.argv[2])) {
     console.log('Missing size');
 } else {
     for (let i = 0; i < parseInt(process.argv[2]); i++) {
-        for (let j = 0; j < parseInt(process.argv[2]); j++) {
-            console.log('X');
-        }
+            console.log('X'.repeat(parseInt(process.argv[2])));
     }
 }
