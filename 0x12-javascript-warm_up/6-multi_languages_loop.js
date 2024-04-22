4df149b00a37a9ae1db109d684d8fa5bca21371a
Rna341 <rna.tariq.aboouf@gmail.com>
2024-04-22 21:07:50 +0200

    Loop print

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints 3 lines using an array of string and a loop
+
+const _array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < _array.length; i++) {
+    console.log(_array[i]);
+}
