8e5652a8888d1664bc1b6190e49e84dc4d87d8e0
Rna341 <rna.tariq.aboouf@gmail.com>
2024-04-22 10:18:02 +0200

    Added shebang

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 // Print statement
 const myVar = "JavaScript is amazing";
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,3 @@
+#!/usr/bin/node
 // Print multiple lines
 console.log("C is fun\nPython is cool\nJavaScript is amazing");
