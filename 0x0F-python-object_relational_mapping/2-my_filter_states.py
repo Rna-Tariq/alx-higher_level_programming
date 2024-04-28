8ac5a8a7f1f58faf24969ed8d8efa9e04af1acb4
Rna341 <rna.tariq.aboouf@gmail.com>
2024-04-28 23:24:14 +0300

    Removed unwanted comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
 
-    # Use format to create the SQL query with user input
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC;"
     cursor.execute(query, (state_name,))
     
